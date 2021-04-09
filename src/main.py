630bb943b3ca27bbf13c8543cef1b2d723c2b85c
Richard Hartmann <hartmri1@fit.cvut.cz>
2021-04-09 19:05:45 +0200

    Create src directory and add main.py file

---
+++ src/main.py
@@ -0,0 +1,13 @@
+import re
+
+example_file = open ("../examples/priklad-optex.tex", "r")
+
+def parse (line):
+    print (line, end="")
+
+
+while (True):
+    line = example_file.readline()
+    if (line == ""):
+        break
+    parse (example_file.readline())
