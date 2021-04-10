6e4e0d08f40e710e18a5c8b11adb6439a3dcf99f
Richard Hartmann <hartmri1@fit.cvut.cz>
2021-04-10 22:40:01 +0200

    Parse words from lines

--- src/main.py
+++ src/main.py
@@ -5,11 +5,33 @@ import keywords as kw
 kw.initialize ()
 
 
-example_file = open ("../examples/priklad-optex.tex", "r")
+example_file = open ("../examples/example1.tex", "r")
 
+
+allwords = []
 
 def parse (line):
-    print (line, end="")
+
+    word_seps = " \t{}();:?!\n"
+    begin = end = 0
+
+    while (begin < len (line)):
+        if (line[begin] in word_seps):
+            begin += 1
+            continue
+
+        end = begin + 1
+
+        while (end < len (line)):
+            if (line [end] in word_seps):
+                break;
+            end += 1
+
+        allwords.append (line[begin:end])
+        begin = end
+
+    print ("Line parsed: ")
+    print (allwords)
 
 
 while (True):
