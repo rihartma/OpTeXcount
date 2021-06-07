e965a59f03ada180638d18477ae0843ef2f65cdf
Richard Hartmann <hartmri1@fit.cvut.cz>
2021-06-07 17:14:13 +0200

    Change the structure of keywords storage

--- src/keywords.py
+++ src/keywords.py
@@ -1,56 +1,73 @@
-"""
-
-We will divide the keywords into sections representing their functionality.
-
-Sections id & description:
-
-1   Keys and keywords that are used for comments
-
-2   Headers - keywords in this section representing keywords that are used to
-    introduce titel, chapter, section etc... It has got specific behaviour. It
-    is terminated by end of line
-    TODO: multiple line header
-
-3   Captions - keywords that are used to describe tables, images etc...
-
-6   Inline formulae - they are generally introduced with $ ... $
-
-7   Displayed formulae - they are generally introduces with $$ ... $$
-
-------------------------------------------------------------------------------
-Note: all keywords in particular sections should behave in same way
-
-"""
-
-# section 1 keywords
-commentary_keys = ["%"]
-
-# section 2 keywords
-headers = ["\\tit", "\\chap", "\\sec", "\\secc"]
-
-# section 3 keywords
-# captions = ["\\caption/t", "\\caption/f"]
-
-# section 6
-# ---
-
-#section 7
-# ---
 
 
-"""
-In the script keywords are used using disctionary because of efficient searching
-structure: { key - keyword: value - id of section }
-"""
-key_words = {}
+#headers = ["\\tit", "\\chap", "\\sec", "\\secc"]
+# blank_keywords = ["\\nl", "\\bye", "\\Blue", "\\Red", "\\Brown", "\\Green", "\\Yellow", "\\Cyan", "\\Magenta", "\\White", "\\Grey", "\\LightGreyand", "\\Black", "\\chyph"]
 
-def initialize ():
 
-    for c in commentary_keys:
-        key_words [c] = 1
+'''
+List of known keywords with their arguments
+empty list - no argument
+"B" - obligatory arguments
+"O" - optional arguments
 
-    for h in headers:
-        key_words [h] = 2
+'''
+all_keywords = {
+    "\\tit":[],
+    "\\chap":[],
+    "\\sec":[],
+    "\\secc":[],
+    "\\maketoc":[],
+    "\\ii":[],
+    "\\makeindex":[],
+    "\\label":[],
+    "\\ref":[],
+    "\\pgref":[],
+    "\\caption/t":[],
+    "\\caption/f":[],
+    "\\eqmark":[],
+    "\\begitems":[],
+    "\\enditems":[],
+    "\\begblock":[],
+    "\\endblock":[],
+    "\\begtt":[],
+    "\\endtt":[],
+    "\\verbchar":[],
+    "\\code":[],
+    "\\verbinput":[],
+    "\\begmulti":[],
+    "\\endmulti":[],
+    "\\cite":[],
+    "\\rcite":[],
+    "\\sortcitations":[],
+    "\\shortcitations":[],
+    "\\nonumcitations":[],
+    "\\bib":[],
+    "\\usebib":[],
+    "\\load":[],
+    "\\fontfam":["O"],
+    "\\typosize":["O"],
+    "\\typoscale":[],
+    "\\thefontsize":[],
+    "\\thefontscale":[],
+    "\\inspic":[],
+    "\\table":[],
+    "\\fnote":[],
+    "\\mnote":[],
+    "\\hyperlinks":[],
+    "\\outlines":[],
+    "\\magscale":[],
+    "\\margins":[],
+    "\\report":[],
+    "\\letter":[],
+    "\\slides":[],
+    "\\chyph":[],
+    "\\style":["B"],
+    "\\novspace":[]
+}
 
-    # for c in captions:
-        # key_words [c] = 3
+# def initialize ():
+
+#     for h in headers:
+#         key_words [h] = headers
+
+#initialize()
