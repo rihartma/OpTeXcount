dc465547be3934bd28e83f0298174819356ee956
Richard Hartmann <hartmri1@fit.cvut.cz>
2021-06-08 17:08:29 +0200

    Update structure of keywords list

--- src/keywords.py
+++ src/keywords.py
@@ -1,73 +1,37 @@
-
-
-#headers = ["\\tit", "\\chap", "\\sec", "\\secc"]
-# blank_keywords = ["\\nl", "\\bye", "\\Blue", "\\Red", "\\Brown", "\\Green", "\\Yellow", "\\Cyan", "\\Magenta", "\\White", "\\Grey", "\\LightGreyand", "\\Black", "\\chyph"]
-
-
 '''
-List of known keywords with their arguments
-empty list - no argument
-"B" - obligatory arguments
-"O" - optional arguments
-
+List of known keywords that has got some arguments
+"O" - strOke argument
+"W" - argument Without brackets
+"S" - argument in Square brackets
+"P" - argument in Paratheses
+"C" - argument in Curly brackets
 '''
-all_keywords = {
-    "\\tit":[],
-    "\\chap":[],
-    "\\sec":[],
-    "\\secc":[],
-    "\\maketoc":[],
-    "\\ii":[],
-    "\\makeindex":[],
-    "\\label":[],
-    "\\ref":[],
-    "\\pgref":[],
-    "\\caption/t":[],
-    "\\caption/f":[],
-    "\\eqmark":[],
-    "\\begitems":[],
-    "\\enditems":[],
-    "\\begblock":[],
-    "\\endblock":[],
-    "\\begtt":[],
-    "\\endtt":[],
-    "\\verbchar":[],
-    "\\code":[],
-    "\\verbinput":[],
-    "\\begmulti":[],
-    "\\endmulti":[],
-    "\\cite":[],
-    "\\rcite":[],
-    "\\sortcitations":[],
-    "\\shortcitations":[],
-    "\\nonumcitations":[],
-    "\\bib":[],
-    "\\usebib":[],
-    "\\load":[],
-    "\\fontfam":["O"],
-    "\\typosize":["O"],
-    "\\typoscale":[],
-    "\\thefontsize":[],
-    "\\thefontscale":[],
-    "\\inspic":[],
-    "\\table":[],
-    "\\fnote":[],
-    "\\mnote":[],
-    "\\hyperlinks":[],
-    "\\outlines":[],
-    "\\magscale":[],
-    "\\margins":[],
-    "\\report":[],
-    "\\letter":[],
-    "\\slides":[],
-    "\\chyph":[],
-    "\\style":["B"],
-    "\\novspace":[]
+keywords_list = {
+    "\\ii":["W","W"],
+    "\\label":["S"],
+    "\\ref":["S"],
+    "\\pgref":["S"],
+    "\\caption":["0"],
+    #"\\verbchar":[],
+    "\\begmulti":["W"],
+    "\\cite":["S"],
+    "\\rcite":["S"],
+    "\\bib":["S"],
+    "\\usebib":["O","P","W"],
+    "\\load":["S"],
+    "\\fontfam":["S"],
+    "\\typosize":["S"],
+    "\\typoscale":["S"],
+    "\\thefontsize":["S"],
+    "\\thefontscale":["S"],
+    "\\inspic":["W"],
+    "\\table":["C","C"],
+    "\\fnote":["C"],
+    "\\mnote":["C"],
+    "\\hyperlinks":["C","C"],
+    "\\outlines":["C"],
+    "\\magscale":["S"],
+    "\\margins":["O","W","P","W"],
+    "\\style":["W"],
+    "\\url":["C"]
 }
-
-# def initialize ():
-
-#     for h in headers:
-#         key_words [h] = headers
-
-#initialize()
