74e92921b412dd40509952578f32f56bd7c33c5a
Richard Hartmann <hartmri1@fit.cvut.cz>
2021-06-12 15:22:17 +0200

    Load filename as argument from command line

---
+++ optexcount.py
@@ -0,0 +1,23 @@
+import sys
+import os.path
+import src.counter
+
+
+def main():
+    if len(sys.argv) != 2 and len(sys.argv) != 3:
+        print("Usage: python3 + " + sys.argv[0] + " path-to-file [-verbose]")
+    elif not os.path.isfile(sys.argv[1]):
+        print(sys.argv[1] + " Invalid filename!")
+    elif len(sys.argv) == 3 and sys.argv[2] != "-verbose":
+        print(sys.argv[2] + " Invalid specifier!")
+    else:
+        if len(sys.argv) == 2:
+            c = src.counter.Counter(sys.argv[1])
+        else:
+            c = src.counter.Counter(sys.argv[1], True)
+        c.run()
+        c.print_result()
+
+
+if __name__ == "__main__":
+    main()
