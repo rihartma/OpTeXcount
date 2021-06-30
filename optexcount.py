a2d7a436de8576b34b90772ede4cf327e3086cbe
Richard Hartmann <hartmri1@fit.cvut.cz>
2021-06-30 16:09:50 +0200

    Use argparse library for arguments parser

--- optexcount.py
+++ optexcount.py
@@ -1,48 +1,38 @@
+#!/usr/bin/env python3
+
 """
 The main file for entire optexcount utility
 Author: Richard Hartmann
-Version: 1.0
+Version: 1.1
 """
 
 
-import sys
-import os.path
+import argparse
 import src.counter
 
 
 def main():
-    if len(sys.argv) < 2 and len(sys.argv) > 4:
-        print("Usage: python3 " + sys.argv[0] + " path-to-file [-verbose] [-set-verbchar=verbatim-character]")
-    elif not os.path.isfile(sys.argv[1]):
-        print(sys.argv[1] + " Invalid filename!")
-    elif len(sys.argv) == 3 and sys.argv[2] != "-verbose" and sys.argv[2][0:-1] != "-set-verbchar=":
-        print(sys.argv[2] + " Invalid specifier!")
-        print("Usage: python3 " + sys.argv[0] + " path-to-file [-verbose] [-set-verbchar=verbatim-character]")
-    elif len(sys.argv) == 4 and sys.argv[2] != "-verbose" and sys.argv[3][0:-1] != "-set-verbchar=":
-        if sys.argv[2] != "-verbose":
-            print(sys.argv[2] + " Invalid specifier!")
-        if sys.argv[3][0:-1] != "-set-verbchar=":
-            print(sys.argv[3] + " Invalid specifier!")
-        print("Usage: python3 + " + sys.argv[0] + " path-to-file [-verbose] [-set-verbchar=verbatim-character]")
-    else:
-        verb_char = None
-        verbose_mode = False
-        if len(sys.argv) == 4:
-            verbose_mode = True
-            verb_char = sys.argv[3][-1]
-        elif len(sys.argv) == 3:
-            if sys.argv[2] == "-verbose":
-                verbose_mode = True
-            else:
-                verb_char = sys.argv[2][-1]
-        try:
-            c = src.counter.Counter(sys.argv[1], verbose_mode, verb_char)
-            c.run()
-            c.print_result()
-        except Exception as err:
-            print()
-            print("Problem occurred!")
-            print(err)
+    parser = argparse.ArgumentParser(description="Word counter of OpTeX documents")
+    parser.add_argument('filename', help='filename of our OpTeX file', type=argparse.FileType('r'))
+    parser.add_argument("-v", "--verbose", help="activate verbose mode", action="store_true")
+    parser.add_argument('-s', '--set-verbchar', help='set implicit inline verbatim character', nargs=1)
+    parser.add_argument('--version', action='version', version='Version 1.1')
+    args = parser.parse_args()
+    if args.set_verbchar:
+        if len(args.set_verbchar[0]) != 1:
+            parser.error("SET_VERBCHAR must be a character")
+            raise argparse.ArgumentTypeError("SET_VERBCHAR must be a character")
+        else:
+            args.set_verbchar = args.set_verbchar[0]
+
+    try:
+        c = src.counter.Counter(args.filename.name, args.verbose, args.set_verbchar)
+        c.run()
+        c.print_result()
+    except Exception as err:
+        print()
+        print("Problem occurred!")
+        print(err)
 
 
 if __name__ == "__main__":
