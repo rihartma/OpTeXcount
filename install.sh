74a65288ef5b168e38e3d3827695315fe7dc0545
Richard Hartmann <hartmri1@fit.cvut.cz>
2021-06-30 17:59:18 +0200

    Add user message to the installation proccess

--- install.sh
+++ install.sh
@@ -2,9 +2,10 @@
 
 set -e
 mv src/optexcount.py src/__main__.py
-zip -j optexcount.zip src/*.py
+zip -j optexcount.zip src/*.py 1> /dev/null
 echo '#!/usr/bin/env python3' | cat - optexcount.zip > optexcount
 chmod +x optexcount
 mv src/__main__.py src/optexcount.py
 rm optexcount.zip
 mv optexcount /usr/bin/optexcount
+echo 'OpTeXcount successfully installed!'
