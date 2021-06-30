e4fb6742ef1cfd6d14d71f3dc8da3acf44ea4c07
Richard Hartmann <hartmri1@fit.cvut.cz>
2021-06-30 22:05:06 +0200

    Fix some potential bugs in installation proccess

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 set -e
-mv src/optexcount.py src/__main__.py
+cp src/optexcount.py src/__main__.py
 zip -j optexcount.zip src/*.py 1> /dev/null
 echo '#!/usr/bin/env python3' | cat - optexcount.zip > optexcount
 chmod +x optexcount
-mv src/__main__.py src/optexcount.py
-rm optexcount.zip
+rm optexcount.zip src/__main__.py
 mv optexcount /usr/bin/optexcount
 echo 'OpTeXcount successfully installed!'
